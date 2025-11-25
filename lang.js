bc5d609cd1d6a18e53b0feb426e0443d9e12aadb
EvergreenTh <maussiontom@gmail.com>
2025-11-25 13:49:24 +0100

    Add files via upload

--- lang.js
+++ lang.js
@@ -23,7 +23,8 @@ const translations = {
 	
 	welcome: "welcome to EvthBound.com !",
 	switchLang: "Please Nintendo don't sue me, I just love Earthbound. Evth.com and EvergreenTh.com were already taken I couldn't do anything ya know. Oh and you can switch language in the menu by the way. just click on the EVTH logo !",
-	soWhat: "SO WHAT ABOUT ME ?",
+	SOWHATaboutme1: "SO WHAT",
+	sowhatABOUTME2: "ABOUT ME ?",
 	descParagraph: "Heyo I'm <b>Tom.</b> I'm also known as <b>EvergreenTh</b> online. I'm into <b>photography</b>, making <b>Game Boy games</b>, <b>music</b>, and just creating cool stuff for fun. I'm currently studying at a <b>cinema school</b>, which fits perfectly since I also enjoy <b>making videos</b>, and pretty much anything <b>creative</b>. When I'm not behind my camera, obsessing with niche image stuff, I like to try new things and jump between different <b><s>obsessions</s></b>, I mean- <b>HOBBIES</b> ... This website is just a little space for me to share what I'm up to. Hey it's cool to have a website don't you think ?",
 	moreAboutMe: "MORE ABOUT ME &#8680;",
 	
@@ -74,7 +75,8 @@ const translations = {
 	
     welcome: "Bienvenue sur EvthBound.com ! ",
 	switchLang: "S’il vous plaît Nintendo, ne me poursuivez pas en justice, j’adore juste Earthbound. Evth.com et EvergreenTh.com étaient déjà pris, je pouvais rien faire vous savez. Ah, et vous pouvez changer la langue dans le menu d’ailleurs. Cliquez juste sur le logo EVTH !",
-	soWhat: "ALORS, QU’EST-CE QU’IL Y A A DIRE SUR MOI ?",
+	SOWHATaboutme1: "ALORS, QUE",
+	sowhatABOUTME2: "DIRE SUR MOI ?",
 	descParagraph: "Heyo moi c'est <b>Tom.</b> Je suis aussi connu sous le nom de <b>EvergreenTh</b> en ligne. Je suis passionné de <b>photographie</b>, je crée des <b>jeux Game Boy</b>, fais de la <b>musique</b>, et j’aime créer des trucs cools pour le fun. J’étudie actuellement dans une <b>école de cinéma</b>, ce qui est parfait pour moi vu que j’adore faire des <b>vidéos</b>, et à peu près tout ce qui est <b>créatif</b>. Quand je ne suis pas derrière ma caméra à m’obséder sur des trucs niche d’image, j’aime essayer de nouvelles choses et changer d'<b><s>obsession</s></b> Enfin, je veux dire… changer de <b>PASSIONS</b>. Ce site est juste un petit espace pour partager ce que je fais. Eh, c’est quand même cool d’avoir un site, vous trouvez pas ?", 
 	moreAboutMe: "EN SAVOIR PLUS &#8680;",
 	
